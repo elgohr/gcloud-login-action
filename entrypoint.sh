c24651d50201feffa2f5301834e912b0b69aa127
Tyler Marshall <tyler.marshall@ripplehealthgroup.com>
2020-01-08 16:24:32 -0800

    Fix input variable reference in entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -e
-echo "${GCLOUD_AUTH}" | base64 -d > "${HOME}/gcloud.json" 
+echo "${INPUT_ACCOUNT_KEY}" | base64 -d > "${HOME}/gcloud.json" 
 gcloud auth activate-service-account --key-file="${HOME}/gcloud.json"
 
 echo "::set-output name=username::oauth2accesstoken"
